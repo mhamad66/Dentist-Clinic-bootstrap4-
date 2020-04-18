f0ca8c1c4cbbcd3473a8dbdfd29aff2fc2f76210
mhamad aboasale <mhamadaboasale@gmail.com>
2020-04-18 12:44:43 +0300

    .

--- js/plugin.js
+++ js/plugin.js
@@ -7,3 +7,17 @@ $('.slider,.carousel-item').height(winH-(upperH + navH)+350);
     });
 
     
+    const btnScrollDown = document.querySelector("#btnScrollDown");
+    btnScrollDown.addEventListener("click",function(){
+        window.scrollTo({
+            top:50,
+            left:0,
+            behavior:'smooth'
+        });
+   
+    });
+
+    $(window).on('load', function () {
+        $("body").css("overflow","auto");
+        $(".lodaingOverlay").hide(1000);
+    });
